119124ec29f23b644728755893208f5f3795f5c4
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2021-04-15 23:29:19 +0530

    Added stocks Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
   devise_for :users
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
